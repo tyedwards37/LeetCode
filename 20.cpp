530c4bd68f4398aa068007ff0e1dd1cac9954f8c
Tyler Edwards <tk.edwards2003@gmail.com>
2023-11-12 13:10:18 -0800

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -1,4 +1,4 @@
-/* LeetCode Day 18: N-th Tribonacci Number (Easy) 20/75 
+/* LeetCode Day 18: Min Cost Climbing Stairs (Easy) 20/75 
  * by Tyler Edwards | November 11th, 2023 
  * Desc: You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
  * You can either start from the step with index 0, or the step with index 1. Return the minimum cost to reach the top of the floor. */

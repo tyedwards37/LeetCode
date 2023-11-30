e1e196cc6f92a8568840165c9e7de620a30d820b
Tyler Edwards <tk.edwards2003@gmail.com>
2023-11-29 23:15:59 -0800

    Add files

---
+++ daily_16.cpp
@@ -0,0 +1,26 @@
+/* Daily Question: 1611. Minimum One Bit Operations to Make Integers Zero
+ * by Tyler Edwards | November 29th, 2023 
+ * Given an integer n, you must transform it into 0 using the following operations any number of times:
+ * - Change the rightmost (0th) bit in the binary representation of n
+ * - Change the ith bit in the binary representation of n if the (i-1)th bit is set to 1 and the (i-2)th through 0th bits are set to 0
+ * Return the minimum number of operations to transform n into 0. */
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n, int answer = 0) 
+    {
+        if (n == 0)
+        {
+            return answer;
+        }
+
+        int b = 1;
+
+        while((b << 1) <= n)
+        {
+            b = b << 1;
+        }
+
+        return minimumOneBitOperations((b >> 1) ^ b ^ n, answer + b);
+    }
+};

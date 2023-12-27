2d022ddc15ec995006e096a277eaf0ebbd8c4ad5
Tyler Edwards <tk.edwards2003@gmail.com>
2023-12-26 20:59:45 -0800

    Add files

---
+++ LeetCode75/61.cpp
@@ -0,0 +1,37 @@
+/* LeetCode Day 56: 162. Find Peak Element(Medium) 61/75 
+ * Tyler Edwards | December 26th, 2023 
+ * Code Credit: asbear
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters. */
+
+class Solution {
+public:
+    const vector<string> pad = 
+    {
+        "", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    vector<string> letterCombinations(string digits) 
+    {
+        if (digits.empty()) return {};
+        vector<string> answer;
+        answer.push_back("");
+
+        for (auto digit : digits)
+        {
+            vector<string> temp;
+            for (auto candidate : pad[digit - '0'])
+            {
+                for (auto s : answer)
+                {
+                    temp.push_back(s + candidate);
+                }
+            }
+
+            answer.swap(temp);
+        }
+
+        return answer;
+    }
+};

ec6320026562985ed155d55f251555de2691db4f
Tyler Edwards <tk.edwards2003@gmail.com>
2024-01-11 23:08:07 -0800

    Add files

---
+++ Daily-Challenge/daily_53.cpp
@@ -0,0 +1,28 @@
+/* Daily Question:  1704. Determine if String Halves Are Alike
+ * by Tyler Edwards | January 11th, 2024 
+ * Code Credit: code_nv
+ * You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+ * Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+ * Return true if a and b are alike. Otherwise, return false. */
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int t=0;
+        for(int i=0;i<s.size();i++)
+        {   
+            s[i]=tolower(s[i]);
+            if(i<s.size()/2)
+            {
+                if(s[i]==97 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117)
+                      t++;                
+            }
+            else
+            {
+                if(s[i]==97 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117)
+                     t--;            
+            }     
+        } 
+       return (t == 0);
+    }
+};

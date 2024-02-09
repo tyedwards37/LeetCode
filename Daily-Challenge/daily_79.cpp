70b0e2ccbbbd42684a3d8ea4d5e5274fa35d8a29
Tyler Edwards <tk.edwards2003@gmail.com>
2024-02-08 19:11:13 -0800

    Add files

---
+++ Daily-Challenge/daily_79.cpp
@@ -0,0 +1,54 @@
+/* Daily Question: 368. Largest Divisible Subset
+ * by Tyler Edwards | February 8th, 2024 
+ * Code Credit: amit_gupta10
+ * Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+ * - answer[i] % answer[j] == 0, or
+ * - answer[j] % answer[i] == 0
+ * If there are multiple solutions, return any of them. */
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        if(nums.size()==0){
+            return nums;
+        }
+        sort(nums.begin(),nums.end());
+        int flag=1;
+        if(nums[0]==1){
+            flag=0;
+        }
+        else{
+            nums.push_back(1);
+            flag=1;
+        }
+        sort(nums.begin(),nums.end());
+        int i,j;
+        int dp[nums.size()][2];
+        for(i=nums.size()-1;i>=0;i--){
+            dp[i][0]=0;
+            dp[i][1]=i;
+            for(j=i+1;j<nums.size();j++){
+                if((nums[j]%nums[i])==0){
+                    if(dp[j][0]>dp[i][0]){
+                        dp[i][0]=dp[j][0];
+                        dp[i][1]=j;
+                    }  
+                }
+            }
+            dp[i][0]++;
+        }
+        i=0;
+        vector<int> t;
+        t.push_back(nums[i]);
+        while(dp[i][1]!=i){
+            i=dp[i][1];
+            t.push_back(nums[i]);
+            
+        }
+        if(flag==1){
+            t.erase(t.begin());
+        }
+        return t;
+        
+    }
+};

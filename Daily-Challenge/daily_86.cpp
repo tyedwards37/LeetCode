ad3616c4f7ab4d317522f267fca9e63daa5918aa
Tyler Edwards <tk.edwards2003@gmail.com>
2024-02-15 19:01:54 -0800

    Add files

---
+++ Daily-Challenge/daily_86.cpp
@@ -0,0 +1,24 @@
+/* Daily Question: 1481. Least Number of Unique Integers after K Removals
+ * by Tyler Edwards | February 15th, 2024 
+ * Code Credit: votrubac
+ * Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements. */
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    unordered_map<int, int> counts;
+    vector<int> heap;
+    for (auto n : arr)
+        ++counts[n];
+    for (auto &p: counts)
+        heap.push_back(p.second);
+    make_heap(begin(heap), end(heap), greater<int>());
+    while (k > 0) {
+        k -= heap.front();
+        pop_heap(begin(heap), end(heap), greater<int>()); 
+        if (k >= 0)
+            heap.pop_back();
+    }
+    return heap.size();
+}
+};

96de7cdb35e2f9e82cc52c5d9e208bc55948b4df
Tyler Edwards <tk.edwards2003@gmail.com>
2024-03-14 23:00:42 -0700

    Add files

---
+++ Daily-Challenge/daily_113.cpp
@@ -0,0 +1,24 @@
+/* Daily Question: 238. Product of Array Except Self
+ * by Tyler Edwards | March 14th, 2024 
+ * Code Credit: its_vishal_7575
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without using the division operation. */
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+};

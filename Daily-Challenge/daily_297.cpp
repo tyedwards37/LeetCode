1b92df8065c446f574a3e0ebcd231d9c9f3d5a5c
Tyler Edwards <tk.edwards2003@gmail.com>
2024-09-15 20:00:19 -0700

    Add files

---
+++ Daily-Challenge/daily_297.cpp
@@ -0,0 +1,28 @@
+/* Daily Question: 539. Minimum Time Difference
+ * by Tyler Edwards | September 15th, 2024 
+ * Code Credit: alexander
+ * Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list. */
+
+ class Solution {
+public:
+    int findMinDifference(vector<string>& times) {
+        int n = times.size();
+        sort(times.begin(), times.end());
+        int mindiff = INT_MAX;
+        for (int i = 0; i < times.size(); i++) {
+            int diff = abs(timeDiff(times[(i - 1 + n) % n], times[i]));
+            diff = min(diff, 1440 - diff);
+            mindiff = min(mindiff, diff);
+        }
+        return mindiff;
+    }
+
+private:
+    int timeDiff(string t1, string t2) {
+        int h1 = stoi(t1.substr(0, 2));
+        int m1 = stoi(t1.substr(3, 2));
+        int h2 = stoi(t2.substr(0, 2));
+        int m2 = stoi(t2.substr(3, 2));
+        return (h2 - h1) * 60 + (m2 - m1);
+    }
+};

7e34bcb14bb76af4b8acb7afb6d475faff2508e4
Tyler Edwards <tk.edwards2003@gmail.com>
2024-10-08 18:10:50 -0700

    Update daily_320.cpp

--- Daily-Challenge/daily_320.cpp
+++ Daily-Challenge/daily_320.cpp
@@ -8,3 +8,18 @@
  * You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
  * - For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
  * Return the minimum number of moves required to make s valid. */
+
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        int left = 0, right = 0;
+        for (char c : S)
+            if (c == '(')
+                right++;
+            else if (right > 0)
+                right--;
+            else
+                left++;
+        return left + right;
+    }
+};

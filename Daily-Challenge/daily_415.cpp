377fef8b0b2cc8b35619be1ea2dc4cb74936c971
Tyler Edwards <tk.edwards2003@gmail.com>
2025-01-12 22:24:11 -0800

    Add files

---
+++ Daily-Challenge/daily_415.cpp
@@ -0,0 +1,29 @@
+/* Daily Question: 3223. Minimum Length of String After Operations
+ * by Tyler Edwards | Janaury 11th, 2025 
+ * Code Credit: Amritesh Chandra
+ * You are given a string s.
+ * You can perform the following process on s any number of times:
+ * - Choose an index i in the string such that there is at least one character to the left of index i that is equal to s[i], and at least one character to the right that is also equal to s[i].   
+ * - Delete the closest character to the left of index i that is equal to s[i].
+ * - Delete the closest character to the right of index i that is equal to s[i].
+ * Return the minimum length of the final string s that you can achieve. */
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> charFrequency(26, 0);
+        int totalLength = 0;
+        for (char currentChar : s) {
+            charFrequency[currentChar - 'a']++;
+        }
+        for (int frequency : charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) {
+                totalLength += 2;
+            } else {
+                totalLength += 1;
+            }
+        }
+        return totalLength;
+    }
+};
